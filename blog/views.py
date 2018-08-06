f270049d51415ec8865bc9c0702812d8cb9013a2
Dante Calderón <dantehemerson@gmail.com>
2018-08-05 22:49:44 -0500

    Created views for index

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 
 def index(request):
 	return HttpResponse("Hello World")
+
+class IndexView(ListView):
+	model = Post
+	template_name = 'base.html'
+	context_object_name = 'post_list'
+	paginate_by = 10
