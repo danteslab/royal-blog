098b7c476759c9b7278e17072b3668917322b26e
Dante Calderón <dantehemerson@gmail.com>
2018-08-05 20:39:30 -0500

    Added convertion from markdown

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
-from django.db import models
+import markdown
 
+from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html	import strip_tags
+
 
 class Category(models.Model):
 	name = models.CharField(max_length=100)
@@ -23,9 +26,9 @@ class Post(models.Model):
 	excerpt = models.CharField(max_length=200, blank=True)
 	# View cunter in publications.
 	views = models.PositiveIntegerField(default=0)
-	category = models.ForeignKey(Category)
+	category = models.ForeignKey(Category, on_delete=models.PROTECT)
 	tags = models.ManyToManyField(Tag, blank=True)
-	author = models.ForeignKey(User)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	class Meta:
 		ordering = ['-created_time']
@@ -39,3 +42,12 @@ class Post(models.Model):
 	def increase_views(self):
 		self.views += 1
 		self.save(update_fields=['views'])
+	
+	def save(self, *args, **kwargs):
+		if not self.excerpt:
+			md = markdown.Markdown(extensions=[
+				'markdown.extensions.extra',
+				'markdown.extensions.codehilite',
+			])
+			self.excerpt = strip_tags(md.convert(self.body))[:54]	
+		super(Post, self).save(*args, **kwargs)
